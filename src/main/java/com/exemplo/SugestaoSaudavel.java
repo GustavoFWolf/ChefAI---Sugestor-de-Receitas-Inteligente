ed0da5c54cf5bcf1c1517296b5dd09c21e44afb6
Gustavo Farinhuk Wolf <gustavofwolf@gmail.com>
2025-12-09 15:37:45 -0300

    Add files via upload

---
+++ src/main/java/com/exemplo/SugestaoSaudavel.java
@@ -0,0 +1,20 @@
+package com.exemplo;
+import com.Ingredientes;
+
+public class SugestaoSaudavel extends Sugestao {
+
+    public SugestaoSaudavel(Ingredientes ingredientes) {
+        super(ingredientes);
+    }
+
+    public void fazerSujestao() {
+        String prompt = "Me diga três receitas de no máximo 30 minutos obrigatoriamente saudaveis" +
+                "com esses ingredientes e quantidades respectivamente: " +
+                ingredientes.viraString() +
+                " você opcinalmente pode usar alho, azeite, sal e cebola.";
+
+        String resposta = model.generate(prompt);
+
+        System.out.println("\nResposta da IA:\n" + resposta);
+    }
+}
